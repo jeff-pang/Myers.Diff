1215433d61c4edd72fec4ac2daf1c5db3a2b9dfe
Jeff Pang <jeff.tspang@gmail.com>
2016-11-05 18:49:20 +0800

    added tests and project

---
+++ Myers.Diff.Tests/DiffTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Myers.Diff.Tests
+{
+    [TestClass]
+    public class DiffTests
+    {
+        [TestMethod]
+        public void basic_greedy_algorithm()
+        {
+            GreedyAlgo algo = new GreedyAlgo();
+            var results = algo.Start("ABCABBA", "CBABAC");
+
+            Vertex[] expected = new Vertex[] {
+                new Vertex(2,'A',0,2),
+                new Vertex(2,'B',1,3),
+                new Vertex(2,'B',1,1),
+                new Vertex(2,'C',2,0),
+                new Vertex(3,'C',2,5),
+                new Vertex(3,'A',3,4),
+                new Vertex(3,'A',3,2),
+                new Vertex(3,'B',4,3),
+                new Vertex(3,'B',4,1),
+                new Vertex(4,'A',6,4),
+                new Vertex(4,'A',6,2)
+            };
+
+            for (int x = 0; x < 11; x++)
+            {
+                Assert.AreEqual(expected[x].D, results[x].D, $"D failed at x={x}");
+                Assert.AreEqual(expected[x].C, results[x].C, $"C failed at x={x}");
+                Assert.AreEqual(expected[x].X, results[x].X, $"X failed at x={x}");
+                Assert.AreEqual(expected[x].Y, results[x].Y, $"Y failed at x={x}");
+            }
+
+        }
+    }
+}
---
+++ Myers.Diff.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Myers.Diff.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Myers.Diff.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("87e66a1c-b655-4299-bb72-28fa2cd626ba")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
